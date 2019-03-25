790bb63f123a8fa2eb36295a1267b585912c7345
Isaac Henry Thiessen <C0448567@intra.camosun.bc.ca>
2019-03-25 09:33:22 -0700

    implemented GridTetromino, untested

---
+++ Lab8/GridTetromino.cpp
@@ -0,0 +1,36 @@
+#include "GridTetromino.h"
+#include "Point.h"
+#include <iostream>
+#include <string>
+
+GridTetromino::GridTetromino() {
+	gridLoc = Point(0, 0);
+}
+
+Point GridTetromino::getGridLoc() const {
+	return gridLoc;
+}
+void GridTetromino::setGridLoc(int x, int y) {
+	gridLoc = Point(x, y);
+}
+void GridTetromino::setGridLoc(const Point& pt) {
+	gridLoc = pt;
+}
+
+void GridTetromino::move(int xOffset, int yOffset) {
+	gridLoc.setXY(gridLoc.getX() + xOffset, gridLoc.getY() + yOffset);
+}
+
+std::vector<Point> GridTetromino::getBlockLocsMappedToGrid() const {
+
+	std::vector<Point> newLocs;
+	std::vector<Point> blockLocs = getBlockLocs();
+
+	for (int i = 0; i < blockLocs.size(); i++) {
+		Point newPt = Point(blockLocs[i].getX() + gridLoc.getX(), blockLocs[i].getY() + gridLoc.getY());
+		newLocs.push_back(newPt);
+	}
+
+	return newLocs;
+
+}
